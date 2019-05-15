eb4f7100e6dd78bb9d7659be3c87df773f718e1f
Anatoly <kypiwindy1@gmail.com>
2019-05-15 19:06:12 +0300

    Add project files.

---
+++ ShData/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using Dapper;
+
+namespace ShData
+{
+    public class DataAccess
+    {
+        public static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+
+        public static void CreateSchema(string sql)
+        {
+            using (IDbConnection con=new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                con.Execute(sql);
+            }
+        }
+    }
+}
---
+++ ShScheduler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using ShData;
+
+namespace ShScheduler
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+             InitDbIfRequired();
+
+            Application.Run(new Form1());
+        }
+
+        static void InitDbIfRequired()
+        {
+            if (!File.Exists("scheduler.db"))
+            {
+                DataAccess.CreateSchema(File.ReadAllText("Schema.sql"));
+            }
+        }
+    }
+}
