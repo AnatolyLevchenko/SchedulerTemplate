799eb8aeaea36b9fbee921d2432d4ae42cb2ba24
Anatoly <kypiwindy1@gmail.com>
2019-05-15 19:43:05 +0300

    Added quartz

--- ShScheduler/Form1.cs
+++ ShScheduler/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Quartz;
+using Quartz.Impl;
 
 namespace ShScheduler
 {
     public partial class Form1 : Form
     {
+        private StdSchedulerFactory schedFact;
+        private  IScheduler Scheduler;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            schedFact = new StdSchedulerFactory();
+            Scheduler = schedFact.GetScheduler().Result;
         }
     }
 }
