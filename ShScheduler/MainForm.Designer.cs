8e6d23b519dd62d8777683e28ec81bceb7fa51be
AnatolyLevchenko <kypiwindy@xaker.ru>
2019-05-16 13:25:21 +0300

    hover color

--- ShScheduler/MainForm.Designer.cs
+++ ShScheduler/MainForm.Designer.cs
@@ -31,47 +31,49 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnShowJobs = new System.Windows.Forms.Button();
             this.btnShowTriggers = new System.Windows.Forms.Button();
-            this._ucJobs1 = new ShScheduler._ucJobs();
             this._ucTriggers1 = new ShScheduler._ucTriggers();
+            this._ucJobs1 = new ShScheduler._ucJobs();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
             this.panel1.Controls.Add(this.btnShowTriggers);
             this.panel1.Controls.Add(this.btnShowJobs);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(84, 495);
+            this.panel1.Size = new System.Drawing.Size(94, 495);
             this.panel1.TabIndex = 8;
             // 
             // btnShowJobs
             // 
-            this.btnShowJobs.Location = new System.Drawing.Point(6, 12);
+            this.btnShowJobs.BackColor = System.Drawing.Color.Khaki;
+            this.btnShowJobs.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnShowJobs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShowJobs.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnShowJobs.Location = new System.Drawing.Point(3, 12);
             this.btnShowJobs.Name = "btnShowJobs";
-            this.btnShowJobs.Size = new System.Drawing.Size(75, 23);
+            this.btnShowJobs.Size = new System.Drawing.Size(88, 37);
             this.btnShowJobs.TabIndex = 0;
             this.btnShowJobs.Text = "Jobs";
-            this.btnShowJobs.UseVisualStyleBackColor = true;
+            this.btnShowJobs.UseVisualStyleBackColor = false;
             this.btnShowJobs.Click += new System.EventHandler(this.btnShowJobs_Click);
             // 
             // btnShowTriggers
             // 
-            this.btnShowTriggers.Location = new System.Drawing.Point(6, 41);
+            this.btnShowTriggers.BackColor = System.Drawing.Color.Khaki;
+            this.btnShowTriggers.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnShowTriggers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShowTriggers.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnShowTriggers.Location = new System.Drawing.Point(3, 55);
             this.btnShowTriggers.Name = "btnShowTriggers";
-            this.btnShowTriggers.Size = new System.Drawing.Size(75, 23);
+            this.btnShowTriggers.Size = new System.Drawing.Size(88, 37);
             this.btnShowTriggers.TabIndex = 1;
             this.btnShowTriggers.Text = "Triggers";
-            this.btnShowTriggers.UseVisualStyleBackColor = true;
+            this.btnShowTriggers.UseVisualStyleBackColor = false;
             this.btnShowTriggers.Click += new System.EventHandler(this.btnShowTriggers_Click);
-            // 
-            // _ucJobs1
-            // 
-            this._ucJobs1.Location = new System.Drawing.Point(114, 12);
-            this._ucJobs1.Name = "_ucJobs1";
-            this._ucJobs1.Size = new System.Drawing.Size(693, 471);
-            this._ucJobs1.TabIndex = 9;
             // 
             // _ucTriggers1
             // 
@@ -80,10 +82,18 @@
             this._ucTriggers1.Size = new System.Drawing.Size(688, 483);
             this._ucTriggers1.TabIndex = 10;
             // 
+            // _ucJobs1
+            // 
+            this._ucJobs1.Location = new System.Drawing.Point(114, 12);
+            this._ucJobs1.Name = "_ucJobs1";
+            this._ucJobs1.Size = new System.Drawing.Size(693, 471);
+            this._ucJobs1.TabIndex = 9;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(840, 495);
             this.Controls.Add(this._ucTriggers1);
             this.Controls.Add(this._ucJobs1);
--- ShScheduler/MainForm.cs
+++ ShScheduler/MainForm.cs
@@ -22,39 +22,10 @@ namespace ShScheduler
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            FillGrids();
             _ucJobs1.BringToFront();
             _ucTriggers1.Visible = false;
+            ChangeBackColor(btnShowJobs);
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-           
-        }
-
-        private void btnAddJob_Click(object sender, EventArgs e)
-        {
-            new AddJob().ShowDialog();
-            FillGrids();
-        }
-
-        private void btnRefresh_Click(object sender, EventArgs e)
-        {
-        
-            FillGrids();
-        }
-
-        void FillGrids()
-        {
-            var jobs = Singleton.Instance.Scheduler.GetJobs();
-            //olvJobs.SetObjects(jobs);
-
-            var triggers = Singleton.Instance.Scheduler.GetAllTriggers();
-            //triggers[0].
-           // olvTriggers.SetObjects(triggers);
-
-        }
-
 
         protected override void Dispose(bool disposing)
         {
@@ -66,20 +37,12 @@ namespace ShScheduler
             base.Dispose(disposing);
         }
 
-        private void btnAddTrigger_Click(object sender, EventArgs e)
-        {
-            new AddTrigger().ShowDialog();
-        }
-
-       
-
-        
-
         private void btnShowJobs_Click(object sender, EventArgs e)
         {
             _ucJobs1.Visible = true;
             _ucJobs1.BringToFront();
             _ucTriggers1.Visible = false;
+            ChangeBackColor(sender);
         }
 
         private void btnShowTriggers_Click(object sender, EventArgs e)
@@ -87,6 +50,21 @@ namespace ShScheduler
             _ucTriggers1.BringToFront();
             _ucJobs1.Visible = false;
             _ucTriggers1.Visible = true;
+
+            ChangeBackColor(sender);
+        }
+
+        void ChangeBackColor(object sender)
+        {
+            if (sender is Button bt)
+            {
+                var buttons = this.panel1.Controls.OfType<Button>();
+                foreach (var button in buttons)
+                {
+                    button.BackColor = bt==button ? Color.DarkOrange : Color.Khaki;
+                }
+            }
+
         }
     }
 }
