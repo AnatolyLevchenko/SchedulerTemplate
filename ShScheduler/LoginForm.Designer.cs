784c9de78276400d500d928d1686e88147f16869
AnatolyLevchenko <kypiwindy@xaker.ru>
2019-05-21 14:08:51 +0300

    login bug

--- ShScheduler/LoginForm.Designer.cs
+++ ShScheduler/LoginForm.Designer.cs
@@ -58,11 +58,8 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(974, 57);
-            this.panel1.TabIndex = 0;
             // 
             // btnClose
             // 
@@ -70,140 +67,90 @@
             this.btnClose.FlatAppearance.BorderSize = 0;
             this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
-            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.btnClose, "btnClose");
             this.btnClose.ForeColor = System.Drawing.Color.White;
             this.btnClose.Image = global::ShScheduler.Properties.Resources.close;
-            this.btnClose.Location = new System.Drawing.Point(929, 12);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(33, 33);
-            this.btnClose.TabIndex = 10;
             this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ShScheduler.Properties.Resources.scheduler;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 4);
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(43, 41);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(1145, 4);
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(43, 41);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 10;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.MouseEnter += new System.EventHandler(this.pictureBox3_MouseEnter);
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.label2, "label2");
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(72, 19);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(154, 26);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Shapl Scheduler";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
+            resources.ApplyResources(this.label1, "label1");
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
-            this.label1.Location = new System.Drawing.Point(252, 251);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 19);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "User Name";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.label3, "label3");
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
-            this.label3.Location = new System.Drawing.Point(380, 222);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(164, 26);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Please Login First";
             // 
             // txtLogin
             // 
-            this.txtLogin.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtLogin.Location = new System.Drawing.Point(256, 282);
+            resources.ApplyResources(this.txtLogin, "txtLogin");
             this.txtLogin.Name = "txtLogin";
-            this.txtLogin.Size = new System.Drawing.Size(400, 31);
-            this.txtLogin.TabIndex = 1;
             // 
             // txtPass
             // 
-            this.txtPass.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPass.Location = new System.Drawing.Point(256, 355);
+            resources.ApplyResources(this.txtPass, "txtPass");
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(400, 31);
-            this.txtPass.TabIndex = 2;
             this.txtPass.UseSystemPasswordChar = true;
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
+            resources.ApplyResources(this.label4, "label4");
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
-            this.label4.Location = new System.Drawing.Point(252, 324);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 19);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Password";
             // 
             // BtnLogin
             // 
             this.BtnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
             this.BtnLogin.FlatAppearance.BorderSize = 0;
-            this.BtnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnLogin.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.BtnLogin, "BtnLogin");
             this.BtnLogin.ForeColor = System.Drawing.Color.White;
-            this.BtnLogin.Location = new System.Drawing.Point(256, 442);
             this.BtnLogin.Name = "BtnLogin";
-            this.BtnLogin.Size = new System.Drawing.Size(400, 34);
-            this.BtnLogin.TabIndex = 4;
-            this.BtnLogin.Text = "Login";
             this.BtnLogin.UseVisualStyleBackColor = false;
             this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::ShScheduler.Properties.Resources.scheduler;
-            this.pictureBox2.Location = new System.Drawing.Point(385, 106);
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(152, 93);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             // 
             // cbRememberMe
             // 
-            this.cbRememberMe.AutoSize = true;
-            this.cbRememberMe.Location = new System.Drawing.Point(256, 409);
+            resources.ApplyResources(this.cbRememberMe, "cbRememberMe");
             this.cbRememberMe.Name = "cbRememberMe";
-            this.cbRememberMe.Size = new System.Drawing.Size(15, 14);
-            this.cbRememberMe.TabIndex = 3;
             this.cbRememberMe.UseVisualStyleBackColor = true;
             // 
             // lbRemember
             // 
-            this.lbRemember.AutoSize = true;
+            resources.ApplyResources(this.lbRemember, "lbRemember");
             this.lbRemember.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
-            this.lbRemember.Location = new System.Drawing.Point(289, 406);
             this.lbRemember.Name = "lbRemember";
-            this.lbRemember.Size = new System.Drawing.Size(108, 19);
-            this.lbRemember.TabIndex = 11;
-            this.lbRemember.Text = "Remember me";
             this.lbRemember.Click += new System.EventHandler(this.lbRemember_Click);
             // 
             // btnEn
@@ -212,14 +159,10 @@
             this.btnEn.FlatAppearance.BorderSize = 0;
             this.btnEn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.btnEn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
-            this.btnEn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEn.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.btnEn, "btnEn");
             this.btnEn.ForeColor = System.Drawing.Color.White;
             this.btnEn.Image = global::ShScheduler.Properties.Resources.uk;
-            this.btnEn.Location = new System.Drawing.Point(27, 118);
             this.btnEn.Name = "btnEn";
-            this.btnEn.Size = new System.Drawing.Size(44, 33);
-            this.btnEn.TabIndex = 12;
             this.btnEn.UseVisualStyleBackColor = false;
             this.btnEn.Click += new System.EventHandler(this.btnEn_Click);
             // 
@@ -229,14 +172,10 @@
             this.btnFr.FlatAppearance.BorderSize = 0;
             this.btnFr.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.btnFr.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
-            this.btnFr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFr.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.btnFr, "btnFr");
             this.btnFr.ForeColor = System.Drawing.Color.White;
             this.btnFr.Image = global::ShScheduler.Properties.Resources.french;
-            this.btnFr.Location = new System.Drawing.Point(77, 118);
             this.btnFr.Name = "btnFr";
-            this.btnFr.Size = new System.Drawing.Size(52, 33);
-            this.btnFr.TabIndex = 13;
             this.btnFr.UseVisualStyleBackColor = false;
             this.btnFr.Click += new System.EventHandler(this.btnEn_Click);
             // 
@@ -245,7 +184,7 @@
             this.AcceptButton = this.BtnLogin;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(974, 549);
+            resources.ApplyResources(this, "$this");
             this.Controls.Add(this.btnFr);
             this.Controls.Add(this.btnEn);
             this.Controls.Add(this.lbRemember);
@@ -258,11 +197,8 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
-            this.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "LoginForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "v";
             this.Load += new System.EventHandler(this.LoginForm_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
