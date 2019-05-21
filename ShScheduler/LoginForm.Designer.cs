0f61f0001e2ca6ea8c8998591358eaf250fc2d40
AnatolyLevchenko <kypiwindy@xaker.ru>
2019-05-21 13:52:18 +0300

    small bug

--- ShScheduler/LoginForm.Designer.cs
+++ ShScheduler/LoginForm.Designer.cs
@@ -109,9 +109,9 @@
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(72, 19);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 26);
+            this.label2.Size = new System.Drawing.Size(154, 26);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Shaple Scheduler";
+            this.label2.Text = "Shapl Scheduler";
             // 
             // label1
             // 
